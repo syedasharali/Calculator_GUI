a92beab12e44a6dee19a77ee032d60bb404a0913
syedasharali <122987639+syedasharali@users.noreply.github.com>
2023-02-01 22:15:31 +0500

    Update calculator new.py

--- calculator new.py
+++ calculator new.py
@@ -1,141 +1,132 @@
 from tkinter import *
 
-root=Tk()
+# Create the main window
+root = Tk()
 root.title('Calculator')
 root.iconbitmap(r'calc.ico')
-root.geometry('408x400')
+root.geometry('456x380')
 root.resizable(0,0)
 
+# Create a StringVar to store the current input
+display_text = StringVar()
 
-entry=StringVar()
+# Define functions for each button
+def insert_digit(digit):
+    """Inserts a digit into the display"""
+    display.insert(END, digit)
 
-def n9():
-    enter.insert(END,9)
-
-def n8():
-    enter.insert(END,8)
-    
-def n7():
-    enter.insert(END,7)
-
-def n6():
-    enter.insert(END,6)
-    
-def n5():
-    enter.insert(END,5)
-
-def n4():
-    enter.insert(END,4)
-    
-def n3():
-    enter.insert(END,3)
-
-def n2():
-    enter.insert(END,2)
-    
-def n1():
-    enter.insert(END,1)
-
-def nplus():
+def insert_zero():
+    """Inserts zero"""
+    if display_text.get()!='0':
+        display.insert(END,0)
+        
+def insert_plus():
     flag=True
-    if entry.get()!='':
-        for i in list('+x/'):
-            if entry.get()[-1]==i:
+    if display_text.get()!='':
+        for i in '+-x/':
+            if display_text.get()[-1]==i:
                 flag=False
         if flag==True:
-            if entry.get()[-1]=='-':
-                enter.delete(len(entry.get())-1,len(entry.get()))
-            enter.insert(END,'+')
-    
-def nminus():
-    if entry.get()!='':
-        if entry.get()[-1]!='-':
-            if entry.get()[-1]=='+':
-                enter.delete(len(entry.get())-1,len(entry.get()))
-            enter.insert(END,'-')
-    else:
-        enter.insert(END,'-')
+            if display_text.get()[-1]=='-':
+                display.delete(len(display_text.get())-1,len(display_text.get()))
+            display.insert(END,'+')
 
-def nx():
-    if entry.get()!='':
-        if entry.get()[-1] in list('+-x/'):
-            enter.delete(len(entry.get())-1,len(entry.get()))
-        enter.insert(END,'x')
-    
-def ndivide():
-    if entry.get()!='':
-        if entry.get()[-1] in list('+-x/'):
-            enter.delete(len(entry.get())-1,len(entry.get()))
-        enter.insert(END,'/')
-        
-def n0():
-    if entry.get()!='0':
-        enter.insert(END,0)
-    
-def ndot():
-    if entry.get()=='.':
-        pass
-    elif entry.get()=='':
-        enter.insert(END,'0.')
-    elif entry.get()[-1]=='.':
-        pass
+def insert_minus():
+    if display_text.get()!='':
+        if display_text.get()[-1]!='-':
+            if display_text.get()[-1]=='+':
+                display.delete(len(display_text.get())-1,len(display_text.get()))
+            display.insert(END,'-')
     else:
-        enter.insert(END,'.')
+        display.insert(END,'-')
 
-def nexp():
-    if entry.get()!='':
-        if entry.get()[-1] not in list('+-x/'):
-            enter.insert(END,'e')
-    
-def nequal(evt):
-    entryget=entry.get()
-    if 'x' in list(entryget):
-        entryget=list(entry.get())
-        (entryget)[entryget.index('x')]='*'
-        entryget=''.join(entryget)
-    res=eval(entryget)
-    if res==int(res):
-        res=int(res)
-    enter.delete(0,END)
-    enter.insert(0,res)
+def insert_multiply():
+    if display_text.get()!='':
+        if display_text.get()[-1] in '+-x/e':
+            display.delete(len(display_text.get())-1,len(display_text.get()))
+        display.insert(END,'x')
+
+def insert_divide():
+    if display_text.get()!='':
+        if display_text.get()[-1] in list('+-x/'):
+            display.delete(len(display_text.get())-1,len(display_text.get()))
+        display.insert(END,'/')
+
+def insert_decimal():
+    """Inserts a decimal point into the display"""
+    if "." not in display_text.get():
+        display.insert(END, ".")
+
+def insert_exponent():
+    """Inserts an exponent into the display"""
+    try:
+        expression = display_text.get()
+        expression = expression.replace("x","*")
+        eval(expression)
+        if display_text.get()[-1] in '+-x/e':
+            display.delete(len(display_text.get())-1,len(display_text.get()))
+        display.insert(END, "e")
+    except:
+        pass
+
+
+def calculate(evt):
+    """Evaluates the expression in the display and shows the result"""
+    expression = display_text.get()
+    expression = expression.replace("x","*")
+    result = eval(expression)
+    display.delete(0, END)
+    display.insert(0, result)
 
 def clear():
-    enter.delete(len(entry.get())-1,len(entry.get()))
-    
+    """Clears the last character from the display"""
+    display.delete(len(display_text.get()) - 1, len(display_text.get()))
+
 def clear_all():
-    enter.delete(0,END)
-    
+    """Clears the entire display"""
+    display.delete(0, END)
 
+# Create the Entry widget for the display and place it at the top
+display = Entry(root, textvariable=display_text, bd=2, font='Helvetica 30')
+display.grid(columnspan=5, padx=5, pady=12, ipady=3)
 
+# Create the button widgets and place them in a grid layout
+button_list = [
+    ("7", lambda: insert_digit(7)),
+    ("8", lambda: insert_digit(8)),
+    ("9", lambda: insert_digit(9)),
+    ("/", insert_divide),
+    ("AC", clear_all),
+    ("4", lambda: insert_digit(4)),
+    ("5", lambda: insert_digit(5)),
+    ("6", lambda: insert_digit(6)),
+    ("x", insert_multiply),
+    ("C", clear),
+    ("1", lambda: insert_digit(1)),
+    ("2", lambda: insert_digit(2)),
+    ("3", lambda: insert_digit(3)),
+    ("+", insert_plus),
+    ("-", insert_minus),
+    ("0", insert_zero),
+    (".", insert_decimal),
+    ("e", insert_exponent),
+    ("=", lambda: calculate(1))
+]
 
+for i, button in enumerate(button_list):
+    button_text, button_command = button
+    button = Button(root, text=button_text, font='Helvetica 20', command=button_command)
+    if i <= 20:
+        button['width']=3
+    if i == 13:  # for the big plus button that spans rows 3 and 4
+        button.grid(row=3, column=3, rowspan=2, ipadx=15, ipady=46, pady=1, padx=1, sticky='ew')
+    elif i == 18: # for the equal button
+        button.grid(row=4, column=4, ipadx=15, ipady=8, pady=1, padx=1, sticky='ew')
+    else:
+        button.grid(row=i // 5 + 1, column=i % 5, ipadx=15, ipady=8, pady=1, padx=1, sticky='ew')
 
-enter=Entry(root,textvariable=entry,bd=2,font='Helvetica 20')
-enter.grid(columnspan=5,padx=5,pady=20,ipadx=35,ipady=8)
+display.focus_set()
+root.bind('<Return>',calculate)
 
-Button(root,text='7',font='Helvetica 20',command=n7).grid(row=1,column=0,ipadx=19,ipady=8,padx=4,pady=2,sticky='ew')
-Button(root,text='8',font='Helvetica 20',command=n8).grid(row=1,column=1,ipadx=19,ipady=8,pady=2,sticky='ew')
-Button(root,text='9',font='Helvetica 20',command=n9).grid(row=1,column=2,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='/',font='Helvetica 20',command=ndivide).grid(row=1,column=3,ipadx=19,ipady=8,pady=2,sticky='ew')
-Button(root,text='C',font='Helvetica 20',command=clear_all).grid(row=1,column=4,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-
-Button(root,text='4',font='Helvetica 20',command=n4).grid(row=2,column=0,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='5',font='Helvetica 20',command=n5).grid(row=2,column=1,ipadx=19,ipady=8,pady=2,sticky='ew')
-Button(root,text='6',font='Helvetica 20',command=n6).grid(row=2,column=2,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='x',font='Helvetica 20',command=nx).grid(row=2,column=3,ipadx=19,ipady=8,pady=2,sticky='ew')
-Button(root,text='<-',font='Helvetica 20',command=clear).grid(row=2,column=4,ipadx=11,ipady=8,padx=1.9,pady=2,sticky='ew')
-
-Button(root,text='1',font='Helvetica 20',command=n1).grid(row=3,column=0,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='2',font='Helvetica 20',command=n2).grid(row=3,column=1,ipadx=19,ipady=8,pady=2,sticky='ew')
-Button(root,text='3',font='Helvetica 20',command=n3).grid(row=3,column=2,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='+',font='Helvetica 20',command=nplus).grid(rowspan=2,row=3,column=3,ipadx=17,ipady=46,pady=2,sticky='ew')
-Button(root,text='-',font='Helvetica 20',command=nminus).grid(row=3,column=4,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-
-Button(root,text='0',font='Helvetica 20',command=n0).grid(row=4,column=0,ipadx=19,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='.',font='Helvetica 20',command=ndot).grid(row=4,column=1,ipadx=21,ipady=8,pady=2,sticky='ew')
-Button(root,text='e',font='Helvetica 20',command=nexp).grid(row=4,column=2,ipadx=17,ipady=8,padx=1.9,pady=2,sticky='ew')
-Button(root,text='=',font='Helvetica 20',command=lambda: nequal(1)).grid(row=4,column=4,ipadx=15.6,ipady=8,padx=1.9,pady=2,sticky='ew')
-
-enter.focus_set()
-root.bind('<Return>',nequal)
 
 root.mainloop()
